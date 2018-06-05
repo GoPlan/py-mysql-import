2c1b5416cce9f9f913b4dc44e0d45b9b4f484b4e
Duc-Anh Le <ducanh.ki2519@gmail.com>
2018-06-05 10:28:48 +0700

    Removed empty spaces

--- __main__.py
+++ __main__.py
@@ -1,11 +1,14 @@
 import argparse
 from .run import App
 
+
 def run(args):
     App.run(args.config)
 
+
 def prepare(args):
     App.prepare()
+
 
 arg_parser = argparse.ArgumentParser(
     description='A MySQL csv import tool')
@@ -13,7 +16,7 @@ arg_parser = argparse.ArgumentParser(
 sub_parsers = arg_parser.add_subparsers(help='sub-command help')
 
 run_parser = sub_parsers.add_parser('run', help='run help')
-run_parser.set_defaults(func=run)                        
+run_parser.set_defaults(func=run)
 run_parser.add_argument('-c', '--config',
                         default=App.DEFAULT_CONFIG_FILE,
                         help='Configuration file')
